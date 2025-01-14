7602ea9e7a8728a4b375b721a855fad8d84db104
Mahmutov Azat <cudraviymalay@gmail.com>
2025-01-14 21:15:21 +0300

    задача готова

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://94.198.50.185:7081/api/users";
-        String response = restTemplate.getForObject(url, String.class);
+        String usersURL = "http://94.198.50.185:7081/api/users";
+        String response = restTemplate.getForObject(usersURL, String.class);
         System.out.println(response);
 
-        ResponseEntity <String> responseEntity = restTemplate.getForEntity(url, String.class);
+        ResponseEntity <String> responseEntity = restTemplate.getForEntity(usersURL, String.class);
         String sessionId = responseEntity.getHeaders().getFirst("Set-Cookie");
         System.out.println(sessionId);
 
@@ -20,7 +20,19 @@ public class Main {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         httpHeaders.set("Cookie", sessionId);
         HttpEntity <User> httpEntity = new HttpEntity<>(user3, httpHeaders);
-        ResponseEntity <String> firstPartOfCode = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
+        ResponseEntity <String> firstPartOfCode = restTemplate.exchange(usersURL, HttpMethod.POST, httpEntity, String.class);
         System.out.println(firstPartOfCode.getBody());
+
+        user3.setName("Thomas");
+        user3.setLastName("Shelby");
+        ResponseEntity <String> secondPartOfCode = restTemplate.exchange(usersURL, HttpMethod.PUT, httpEntity, String.class);
+        System.out.println(secondPartOfCode.getBody());
+
+        String deleteUserUrl = "http://94.198.50.185:7081/api/users/3";
+        ResponseEntity<String> thirdPartOfCode = restTemplate.exchange(deleteUserUrl, HttpMethod.DELETE, httpEntity, String.class);
+        System.out.println(thirdPartOfCode.getBody());
+        System.out.println("------");
+        String fullCode = firstPartOfCode.getBody() + secondPartOfCode.getBody() + thirdPartOfCode.getBody();
+        System.out.println(fullCode);
     }
 }
